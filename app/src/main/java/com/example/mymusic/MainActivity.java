c28cc985dae1aed07995d9886ecb3708cd097694
Kosey <a22300871@ceti.mx>
2025-05-30 16:48:37 -0600

    Elementos agregados: 1. Diseño de la toolbar 2. Menu overflow 3. Diseño basico del main 4. En MainActivity.java se agrego los metodos necesarios para el menu junto con la opción logout.

--- app/src/main/java/com/example/mymusic/MainActivity.java
+++ app/src/main/java/com/example/mymusic/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.mymusic;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    SharedPreferences archivo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        archivo = this.getSharedPreferences("sesion", Context.MODE_PRIVATE);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.Principal){
+            Toast.makeText(this, "Ya se encuentra aquí.", Toast.LENGTH_SHORT).show();
+        }else if(item.getItemId() == R.id.logOut){
+            if(archivo.contains("id_usuario")){
+                Intent cerrar = new Intent(this, Inicio.class);
+                SharedPreferences.Editor editor = archivo.edit();
+                editor.remove("id_usuario");
+                editor.apply();
+                startActivity(cerrar);
+                finish();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
